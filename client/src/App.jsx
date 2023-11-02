45b102c6c88ad26b1f971602b81b79c1cc66ab04
kara sealeaf <kara.sealeaf13@gmail.com>
2023-11-02 11:57:43 +0000

    app jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  //store our from and to languages in state
+  //store the word we want to translate in state
+
+  //on change function for the input of the work we want to translate
+  //this function will also call out API and get the translation
+
+  //on submit funtion that calls our API to get the translation
+
+  return (
+    <>
+      {/*build our form */}
+      {/*Show our translation*/}
+      {/*STRETCH: show a gif from the GIPHY API that matches the translation */}
+    </>
+  );
 }
 
 export default App;
