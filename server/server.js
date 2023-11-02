a7f99baf5f5db8bc95c0001bf0b1fd7ba7b24610
kara sealeaf <kara.sealeaf13@gmail.com>
2023-11-02 11:48:48 +0000

    skeleton

--- server/server.js
+++ server/server.js
@@ -4,8 +4,30 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
-const axion = require("axios");
+const axios = require("axios");
 
 app.get("/", (_, response) => response.json("Root route for translatim."));
+
+//translate?word=hello&from=en&to=es
+
+app.get("/translate", async (request, response) => {
+  //const word = request.query.word;
+  //const from = request.query.from;
+  //const to = request.query.to;
+
+  // destructure all the properties from request.query into variables
+  const { word, from, to } = request.query;
+
+  // make our API call
+  const API = `https://api.mymemory.translated.net/get?q=${word}&langpair=${from}|${to}`;
+  const res = await axios.get(API);
+
+  const wrangledData = {
+    translation: res.data.responseData.translatedText,
+    match: res.data.responseData.match,
+  };
+
+  response.json(wrangledData);
+});
 
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
