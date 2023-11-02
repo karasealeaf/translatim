53d0bae3c1b62b75fc7e0909a21268c386fb2ad7
kara sealeaf <kara.sealeaf13@gmail.com>
2023-11-02 13:57:05 +0000

    added functions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,59 @@
 import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
   //store our from and to languages in state
+  const [from, setFrom] = useState("ar");
+  const [to, setTo] = useState("ar");
   //store the word we want to translate in state
+  const [word, setWord] = useState("");
+  const [translation, setTranslation] = useState({});
+  //on change function for the from and to states
 
   //on change function for the input of the work we want to translate
+  /*handleChange;*/
   //this function will also call out API and get the translation
 
   //on submit funtion that calls our API to get the translation
+  async function handleTranslate(event) {
+    event.preventDefault();
+    const API = `http://localhost:8080/translate?word=${word}&from=${from}&to=${to}`;
+    const res = await axios.get(API);
+  }
 
   return (
     <>
-      {/*build our form */}
+      <form onSubmit={handleTranslate}>
+        <div className="container">
+          <select onChange={(event) => setFrom(event.target.value)}>
+            <option value="ar">Arabic</option>
+            <option value="en">English</option>
+            <option value="fr">French</option>
+            <option value="ko">Korean</option>
+            <option value="pl">Polish</option>
+            <option value="es">Spanish</option>
+            <option value="tr">Turkish</option>
+          </select>
+          <input
+            placeholder="Translate"
+            onChange={(event) => setWord(event.target.value)}
+          />
+        </div>
+
+        <div className="container">
+          <select onChange={(event) => setTo(event.target.value)}>
+            <option value="ar">Arabic</option>
+            <option value="en">English</option>
+            <option value="fr">French</option>
+            <option value="ko">Korean</option>
+            <option value="pl">Polish</option>
+            <option value="es">Spanish</option>
+            <option value="tr">Turkish</option>
+          </select>
+          <input placeholder="Translate" />
+        </div>
+      </form>
       {/*Show our translation*/}
       {/*STRETCH: show a gif from the GIPHY API that matches the translation */}
     </>
